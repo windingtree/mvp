7928c7931082c2b6f169dcaab2fe24fc3fba2717
Kostiantyn Smyrnov <kostysh@gmail.com>
2024-01-18 12:40:32 +0100

    feat: 🎸 Added airplanes management components

--- packages/dapp-supplier/src/components/Airplanes/AddAirplane.tsx
+++ packages/dapp-supplier/src/components/Airplanes/AddAirplane.tsx
@@ -1,68 +1,155 @@
 import { useCallback, useEffect, useMemo, useState } from 'react';
 import {
+  Alert,
   Button,
   CircularProgress,
   FormLabel,
   Stack,
   TextField,
   Typography,
+  Modal,
+  Card,
+  IconButton,
 } from '@mui/material';
+import { Close as CloseIcon } from '@mui/icons-material';
 import { useForm } from '@conform-to/react';
 import { parse } from '@conform-to/zod';
 import {
   AirplaneConfiguration,
   AirplaneConfigurationSchema,
   AirplaneMedia,
+  AirplaneMeta,
   PriceOption,
 } from './type.js';
-import { ConfigActions, useConfig } from '@windingtree/sdk-react/providers';
-import { CustomConfig } from '../../main.js';
+import { useNode } from '@windingtree/sdk-react/providers';
 import { Gallery } from './Gallery.js';
 import { Prices } from './Prices.js';
+import { AppRouter } from '@windingtree/mvp-node';
+import { inferProcedureInput } from '@trpc/server';
 
-export const AddAirplane = () => {
-  const { cacheAirplane, setConfig } = useConfig<CustomConfig>();
+type AirplanesAddInput = inferProcedureInput<AppRouter['airlines']['add']>;
+
+interface AddAirplaneProps {
+  show?: boolean;
+  onDone?: () => void;
+  record?: AirplaneMeta;
+}
+
+export const AddAirplane = ({
+  show = true,
+  record,
+  onDone = () => {},
+}: AddAirplaneProps) => {
+  const { node } = useNode<AppRouter>();
+  const [values, setValues] = useState<AirplaneConfiguration | undefined>();
   const [prices, setPrices] = useState<PriceOption[]>([]);
   const [gallery, setGallery] = useState<AirplaneMedia[]>([]);
   const [isLoading, setIsLoading] = useState<boolean>(false);
+  const [error, setError] = useState<string | undefined>();
+
+  useEffect(() => {
+    if (record) {
+      setValues(record.config);
+      setPrices(record.prices);
+      setGallery(record.media);
+    }
+  }, [record]);
+
+  const handleAdd = useCallback(
+    async (record: AirplanesAddInput) => {
+      try {
+        setIsLoading(true);
+
+        if (!node) {
+          throw new Error('Not connected to the node');
+        }
+
+        await node.airlines.add.mutate(record);
+        setIsLoading(false);
+      } catch (err) {
+        setIsLoading(true);
+        setError((err as Error).message ?? 'Unknown airplanes addition error');
+      }
+    },
+    [node],
+  );
+
+  const handleUpdate = useCallback(
+    async (id: string, record: AirplanesAddInput) => {
+      try {
+        setIsLoading(true);
+
+        if (!node) {
+          throw new Error('Not connected to the node');
+        }
+
+        await node.airlines.update.mutate({
+          id,
+          data: record,
+        });
+        setIsLoading(false);
+      } catch (err) {
+        setIsLoading(true);
+        setError((err as Error).message ?? 'Unknown airplanes update error');
+      }
+    },
+    [node],
+  );
 
   const [form, fieldset] = useForm<AirplaneConfiguration>({
-    defaultValue: cacheAirplane,
     shouldValidate: 'onBlur',
     onValidate({ formData }) {
       const submission = parse(formData, {
         schema: AirplaneConfigurationSchema,
       });
-      setConfig({
-        type: ConfigActions.SET_CONFIG,
-        payload: {
-          cacheAirplane: submission.payload as AirplaneConfiguration,
-        },
-      });
       return submission;
     },
     onSubmit(e, { submission }) {
       e.preventDefault();
-      setIsLoading(true);
-      console.log('@@@', submission);
-      console.log('===', gallery, prices);
-      // TODO Send request to the Node
-      // TODO Handle Node response
+
+      if (!node) {
+        setError('Not connected to the Node yet');
+        return;
+      }
 
       setIsLoading(true);
+      // console.log('@@@', submission);
+      // console.log('===', gallery, prices);
+
+      const data: AirplanesAddInput = {
+        name: submission.value.name,
+        description: submission.value.description,
+        capacity: submission.value.capacity,
+        minTime: submission.value.minTime,
+        maxTime: submission.value.maxTime,
+        media: gallery,
+        price: prices.map((p) => ({
+          value: BigInt(p.price).toString(),
+          token: p.coin.address,
+        })),
+      };
+
+      console.log('Record ===', data);
+
+      if (record?.id) {
+        handleUpdate(record.id, data).then(() => {
+          handleReset();
+          onDone();
+        });
+      } else {
+        handleAdd(data).then(() => {
+          handleReset();
+          onDone();
+        });
+      }
     },
   });
 
   const handleReset = useCallback(() => {
-    setConfig({
-      type: ConfigActions.SET_CONFIG,
-      payload: {
-        cacheAirplane: undefined,
-      },
-    });
+    setValues(undefined);
     setGallery([]);
     setPrices([]);
-  }, [setConfig]);
+  }, []);
 
   const isReady = useMemo(
     () => form.errors.length === 0 && gallery.length > 0 && prices.length > 0,
@@ -73,115 +160,170 @@ export const AddAirplane = () => {
     console.log('%%% new Gal', gallery);
   }, [gallery]);
 
+  if (!show) {
+    return null;
+  }
+
   return (
     <>
-      <form {...form.props}>
-        <Stack spacing={4} sx={{ marginBottom: 6 }}>
-          <header>
-            <Typography variant="h6" component="h1">
-              Airplane configuration
-            </Typography>
-            <Typography variant="subtitle1">
-              Using this for you are able to add or update airplane in the Node
-              database
-            </Typography>
-          </header>
-
-          <FormLabel>
-            <Typography variant="h6">General Configuration</Typography>
-          </FormLabel>
-
-          <TextField
-            label="Airplane Type"
-            type="text"
-            name="name"
-            placeholder="Copy your airplane type/name here"
-            required
-            disabled={isLoading}
-            error={Boolean(fieldset.name.error)}
-            helperText={fieldset.name.error}
-          />
-          <TextField
-            label="Airplane Details"
-            type="text"
-            name="description"
-            placeholder="Copy your airplane details here (markdown accepted)"
-            multiline
-            rows={4}
-            required
-            disabled={isLoading}
-            error={Boolean(fieldset.description.error)}
-            helperText={fieldset.description.error}
-          />
-          <Stack direction="row" spacing={4}>
-            <TextField
-              label="Airplane Capacity"
-              type="number"
-              name="capacity"
-              placeholder="Copy your the maximum number of airplane passengers"
-              required
-              disabled={isLoading}
-            />
-          </Stack>
-
-          <Stack direction="row" spacing={4}>
-            <TextField
-              label="Minimum Flight Duration"
-              type="number"
-              name="minTime"
-              placeholder="Copy your the minimum duration of flight in minutes"
-              required
-              disabled={isLoading}
-              error={Boolean(fieldset.minTime.error)}
-              helperText={fieldset.minTime.error}
-            />
-            <TextField
-              label="Maximum Flight Duration"
-              type="number"
-              name="maxTime"
-              placeholder="Copy your the maximum duration of flight in minutes"
-              required
-              disabled={isLoading}
-              error={Boolean(fieldset.maxTime.error)}
-              helperText={fieldset.maxTime.error}
-            />
-          </Stack>
-
-          <Gallery
-            gallery={gallery}
-            onChange={setGallery}
-            disabled={isLoading}
-          />
-
-          <Prices prices={prices} onChange={setPrices} disabled={isLoading} />
-
-          <Stack
-            direction="row"
-            justifyContent="flex-end"
-            spacing={2}
-            sx={{ paddingTop: 4 }}
+      <Modal
+        open={show}
+        onClose={onDone}
+        sx={{
+          display: 'flex',
+          alignItems: 'center',
+          justifyContent: 'center',
+          paddingTop: 2,
+        }}
+      >
+        <Card
+          sx={{
+            width: '95vw',
+            maxHeight: '86vh',
+            padding: 2,
+            overflowY: 'auto',
+          }}
+        >
+          <IconButton
+            sx={{
+              position: 'absolute',
+              top: 8,
+              right: 8,
+              zIndex: 1,
+              bgcolor: 'white',
+            }}
+            size="small"
+            onClick={() => {
+              handleReset();
+              onDone();
+            }}
           >
-            <Button
-              type="reset"
-              variant="outlined"
-              disabled={isLoading}
-              onClick={handleReset}
-            >
-              Reset
-            </Button>
-            <Button
-              type="submit"
-              variant="contained"
-              disabled={isLoading || !isReady}
-            >
-              <Stack direction="row" spacing={2} alignItems="center">
-                <Typography>Submit</Typography>
-                {isLoading && <CircularProgress color="inherit" size={16} />}
+            <CloseIcon />
+          </IconButton>
+          <form {...form.props}>
+            <Stack spacing={4} sx={{ marginBottom: 2 }}>
+              <header>
+                <Typography variant="h6" component="h1">
+                  Airplane configuration {record?.id && `(#${record.id})`}
+                </Typography>
+                <Typography variant="subtitle1">
+                  Using this for you are able to add or update airplane in the
+                  Node database
+                </Typography>
+              </header>
+
+              <FormLabel>
+                <Typography variant="h6">General Configuration</Typography>
+              </FormLabel>
+
+              <TextField
+                label="Airplane Type"
+                type="text"
+                name="name"
+                placeholder="Copy your airplane type/name here"
+                required
+                defaultValue={values?.name}
+                disabled={isLoading}
+                error={Boolean(fieldset.name.error)}
+                helperText={fieldset.name.error}
+              />
+              <TextField
+                label="Airplane Details"
+                type="text"
+                name="description"
+                placeholder="Copy your airplane details here (markdown accepted)"
+                multiline
+                rows={4}
+                required
+                defaultValue={values?.description}
+                disabled={isLoading}
+                error={Boolean(fieldset.description.error)}
+                helperText={fieldset.description.error}
+              />
+              <Stack direction="row" spacing={4}>
+                <TextField
+                  label="Airplane Capacity"
+                  type="number"
+                  name="capacity"
+                  placeholder="Copy your the maximum number of airplane passengers"
+                  required
+                  defaultValue={values?.capacity}
+                  disabled={isLoading}
+                />
               </Stack>
-            </Button>
-          </Stack>
-        </Stack>
-      </form>
+
+              <Stack direction="row" spacing={4}>
+                <TextField
+                  label="Minimum Flight Duration"
+                  type="number"
+                  name="minTime"
+                  placeholder="Copy your the minimum duration of flight in minutes"
+                  required
+                  defaultValue={values?.minTime}
+                  disabled={isLoading}
+                  error={Boolean(fieldset.minTime.error)}
+                  helperText={fieldset.minTime.error}
+                />
+                <TextField
+                  label="Maximum Flight Duration"
+                  type="number"
+                  name="maxTime"
+                  placeholder="Copy your the maximum duration of flight in minutes"
+                  required
+                  defaultValue={values?.maxTime}
+                  disabled={isLoading}
+                  error={Boolean(fieldset.maxTime.error)}
+                  helperText={fieldset.maxTime.error}
+                />
+              </Stack>
+
+              <Gallery
+                gallery={gallery}
+                onChange={setGallery}
+                disabled={isLoading}
+              />
+
+              <Prices
+                prices={prices}
+                onChange={setPrices}
+                disabled={isLoading}
+              />
+
+              <Stack
+                direction="row"
+                justifyContent="flex-end"
+                spacing={2}
+                sx={{ paddingTop: 4 }}
+              >
+                <Button
+                  type="reset"
+                  variant="outlined"
+                  disabled={isLoading}
+                  onClick={handleReset}
+                >
+                  Reset
+                </Button>
+                <Button
+                  type="submit"
+                  variant="contained"
+                  disabled={isLoading || !isReady}
+                >
+                  <Stack direction="row" spacing={2} alignItems="center">
+                    <Typography>{record ? 'Update' : 'Add'}</Typography>
+                    {isLoading && (
+                      <CircularProgress color="inherit" size={16} />
+                    )}
+                  </Stack>
+                </Button>
+              </Stack>
+            </Stack>
+          </form>
+
+          <Stack spacing={4} sx={{ marginBottom: 2 }}></Stack>
+
+          {error && <Alert severity="error">{error}</Alert>}
+        </Card>
+      </Modal>
     </>
   );
 };
---
+++ packages/dapp-supplier/src/components/Airplanes/AirplanesList.tsx
@@ -0,0 +1,173 @@
+import { Alert, Grid, Typography, IconButton, Stack } from '@mui/material';
+import { Edit as EditIcon, Delete as DeleteIcon } from '@mui/icons-material';
+import { AppRouter } from '@windingtree/mvp-node';
+import { useNode } from '@windingtree/sdk-react/providers';
+import { useCallback, useEffect, useState } from 'react';
+import { inferProcedureOutput } from '@trpc/server';
+import { AirplaneMeta, PriceOption } from './type.js';
+import { useProtocolConfig } from '../../hooks/useProtocolConfig.js';
+
+type AirplanesResponseGetAll = inferProcedureOutput<
+  AppRouter['airlines']['getAll']
+>;
+
+interface AirplanesListProps {
+  version?: number;
+  onSelected: (data: AirplaneMeta) => void;
+}
+
+export const AirplanesList = ({
+  version = 1,
+  onSelected,
+}: AirplanesListProps) => {
+  const { node, nodeConnected } = useNode<AppRouter>();
+  const { stableCoins } = useProtocolConfig();
+  const [records, setRecords] = useState<AirplanesResponseGetAll>([]);
+  const [error, setError] = useState<string | undefined>();
+
+  const handleGetAirplanes = useCallback(async () => {
+    try {
+      setError(undefined);
+
+      if (!node) {
+        return;
+      }
+
+      const response = await node.airlines.getAll.query();
+      setRecords(response);
+    } catch (err) {
+      setError((err as Error).message ?? 'Unknown airplanes list error');
+    }
+  }, [node]);
+
+  const handleDelete = useCallback(
+    async (id?: string) => {
+      try {
+        if (!node) {
+          throw new Error('Not connected to the node');
+        }
+
+        if (!id) {
+          throw new Error('Invalid configuration. Record Id not found');
+        }
+
+        await node.airlines.delete.mutate(id);
+        handleGetAirplanes();
+      } catch (err) {
+        setError((err as Error).message ?? 'Unknown airplanes delete error');
+      }
+    },
+    [handleGetAirplanes, node],
+  );
+
+  const handleSelect = useCallback(
+    (record: AirplanesResponseGetAll[number]) => {
+      onSelected({
+        id: record.id,
+        config: {
+          name: record.name,
+          description: record.description,
+          capacity: record.capacity,
+          minTime: record.minTime,
+          maxTime: record.maxTime,
+        },
+        media: record.media,
+        prices: record.price.reduce((a, v) => {
+          const coin = stableCoins.find((c) => c.address === v.token);
+
+          if (coin) {
+            return [
+              ...a,
+              {
+                price: BigInt(v.value),
+                coin,
+              },
+            ];
+          }
+
+          return a;
+        }, [] as PriceOption[]),
+      });
+    },
+    [onSelected, stableCoins],
+  );
+
+  useEffect(() => {
+    if (node) {
+      handleGetAirplanes();
+    }
+  }, [node, handleGetAirplanes, version]);
+
+  if (!nodeConnected && records.length === 0) {
+    return (
+      <>
+        <Typography variant="h6">
+          Unable to get the list of airplanes. Not connected to the Node.
+        </Typography>
+      </>
+    );
+  }
+
+  return (
+    <>
+      {records.length === 0 && (
+        <Grid container spacing={2}>
+          <Grid item sx={{ padding: 10, textAlign: 'center' }}>
+            <Typography variant="body1">Airplanes list is empty</Typography>
+          </Grid>
+        </Grid>
+      )}
+
+      {records.length > 0 && (
+        <>
+          <Grid container spacing={2}>
+            <Grid item xs={4} sx={{ padding: 2, textAlign: 'left' }}>
+              <Typography variant="caption">Name (type)</Typography>
+            </Grid>
+            <Grid item xs={2} sx={{ padding: 2, textAlign: 'center' }}>
+              <Typography variant="caption">Capacity</Typography>
+            </Grid>
+            <Grid item xs={2} sx={{ padding: 2, textAlign: 'center' }}>
+              <Typography variant="caption">Min hours</Typography>
+            </Grid>
+            <Grid item xs={2} sx={{ padding: 2, textAlign: 'center' }}>
+              <Typography variant="caption">Max hours</Typography>
+            </Grid>
+            <Grid item xs={2} sx={{ padding: 2, textAlign: 'center' }}>
+              <Typography variant="caption">Action</Typography>
+            </Grid>
+          </Grid>
+
+          {records.map((r, i) => (
+            <Grid key={i} container spacing={2}>
+              <Grid item xs={4} sx={{ padding: 2, textAlign: 'left' }}>
+                <Typography variant="body1">{r.name}</Typography>
+              </Grid>
+              <Grid item xs={2} sx={{ padding: 2, textAlign: 'center' }}>
+                <Typography variant="body1">{r.capacity}</Typography>
+              </Grid>
+              <Grid item xs={2} sx={{ padding: 2, textAlign: 'center' }}>
+                <Typography variant="body1">{r.minTime}</Typography>
+              </Grid>
+              <Grid item xs={2} sx={{ padding: 2, textAlign: 'center' }}>
+                <Typography variant="body1">{r.maxTime}</Typography>
+              </Grid>
+              <Grid item xs={2} sx={{ padding: 2, textAlign: 'center' }}>
+                <Stack direction="row" spacing={2}>
+                  <IconButton size="small" onClick={() => handleSelect(r)}>
+                    <EditIcon />
+                  </IconButton>
+                  <IconButton size="small" onClick={() => handleDelete(r.id)}>
+                    <DeleteIcon />
+                  </IconButton>
+                </Stack>
+              </Grid>
+            </Grid>
+          ))}
+        </>
+      )}
+
+      {error && <Alert severity="error">{error}</Alert>}
+    </>
+  );
+};
