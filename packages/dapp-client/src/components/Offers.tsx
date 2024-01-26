cef547e217df5fc8958bddbd2435197d39042d5c
Kostiantyn Smyrnov <kostysh@gmail.com>
2024-01-26 20:38:26 +0100

    refactor: 💡 useRequests hook refactored into SearchProvider

--- packages/dapp-client/src/components/Offers.tsx
+++ packages/dapp-client/src/components/Offers.tsx
@@ -5,6 +5,8 @@ import {
   CardContent,
   Typography,
   CircularProgress,
+  Stack,
+  Box,
 } from '@mui/material';
 import { OfferOptions } from '@windingtree/mvp-node/types';
 import { OfferData } from '@windingtree/sdk-types';
@@ -15,9 +17,15 @@ import { RequestQuery } from 'mvp-shared-files';
 
 interface OffersProps {
   offers?: OfferData<RequestQuery, OfferOptions>[];
+  subscribed: boolean;
+  onStop: () => void;
 }
 
-export const Offers = ({ offers }: OffersProps) => {
+export const Offers = ({
+  offers,
+  subscribed,
+  onStop = () => {},
+}: OffersProps) => {
   if (!offers || (offers && offers.length === 0)) {
     return null;
   }
@@ -42,18 +50,45 @@ export const Offers = ({ offers }: OffersProps) => {
               title={item.options.airplane.name}
             />
             <CardContent>
-              <Typography gutterBottom variant="h5" component="div">
-                {item.options.airplane.name}
-              </Typography>
-              <Typography variant="body2" color="text.secondary">
-                {item.options.airplane.description}
-              </Typography>
+              <Stack direction="row" spacing={2}>
+                <Box sx={{ paddingRight: 1, borderRight: 1 }}>
+                  <Typography gutterBottom variant="h5" component="div">
+                    {item.options.airplane.name}
+                  </Typography>
+                </Box>
+                <Box>
+                  <Typography variant="h6" color="text.secondary">
+                    {item.options.date}
+                  </Typography>
+                  <Typography variant="h6" color="text.secondary">
+                    Hours: {item.options.hours}
+                  </Typography>
+                </Box>
+              </Stack>
             </CardContent>
           </Card>
         </Grid>
       ))}
       <Grid item xs={12} sm={6} md={4}>
-        <CircularProgress size={20} />
+        {subscribed && (
+          <Card
+            sx={{
+              cursor: 'pointer',
+              display: 'flex',
+              flexDirection: 'column',
+              alignItems: 'center',
+              justifyContent: 'center',
+              maxWidth: 300,
+              height: '100%',
+            }}
+            onClick={onStop}
+            variant="outlined"
+          >
+            <CardContent>
+              <CircularProgress title="Stop" size={80} thickness={1} />
+            </CardContent>
+          </Card>
+        )}
       </Grid>
     </Grid>
   );
--- packages/dapp-client/src/pages/SearchPage.tsx
+++ packages/dapp-client/src/pages/SearchPage.tsx
@@ -8,27 +8,33 @@ import {
   Button,
   CardContent,
   Typography,
+  useMediaQuery,
 } from '@mui/material';
+import { useTheme } from '@mui/material/styles';
 import dayjs, { Dayjs } from 'dayjs';
 import { DatePicker } from '@mui/x-date-pickers';
 import { useSearchParams } from 'react-router-dom';
 import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
 import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
 import { useNavigate } from 'react-router-dom';
-import { useRequests } from '../hooks/useRequests.js';
-import { mainShowcase, requestExpiration, nodeTopic } from '../config.js';
+import { mainShowcase } from '../config.js';
 import { OfferOptions } from '@windingtree/mvp-node/types';
 import { RequestQuery } from 'mvp-shared-files';
 import { ClientRequestRecord } from '@windingtree/sdk-client';
 import { Offers } from '../components/Offers.js';
 import { createLogger } from '@windingtree/sdk-logger';
+import { useClient } from '@windingtree/sdk-react/providers';
+import { useSearchProvider } from '../providers/SearchProvider/SearchProviderContext.js';
 
 const logger = createLogger('SearchPage');
 
 export const SearchPage = () => {
+  const theme = useTheme();
+  const isMobile = useMediaQuery(theme.breakpoints.down('sm'));
   const [searchParams] = useSearchParams();
   const navigate = useNavigate();
   const tourId = searchParams.get('tour');
+  const requestId = searchParams.get('requestId');
   const tour = useMemo(
     () => mainShowcase.find((t) => t.id === tourId),
     [tourId],
@@ -37,18 +43,16 @@ export const SearchPage = () => {
     dayjs(new Date()),
   );
   const [tourError, setTourError] = useState<string | undefined>();
+  const { clientConnected } = useClient();
   const {
-    clientConnected,
     requestsManager,
+    requests,
     publish,
     error: requestsError,
-  } = useRequests({
-    expire: requestExpiration,
-    topic: nodeTopic,
-  });
-  const [currentRequest, setCurrentRequest] = useState<
+  } = useSearchProvider();
+  const currentRequest = useMemo<
     ClientRequestRecord<RequestQuery, OfferOptions> | undefined
-  >();
+  >(() => requests.find((r) => r.data.id === requestId), [requestId, requests]);
 
   useEffect(() => {
     if (!tour) {
@@ -58,17 +62,11 @@ export const SearchPage = () => {
     }
   }, [tourId, tour]);
 
-  const currentOffers = useMemo(
-    () => currentRequest?.offers ?? [],
-    [currentRequest?.offers],
-  );
-
   const handlePublish = useCallback(async () => {
     try {
       if (selectedDate) {
         if (requestsManager && currentRequest) {
           requestsManager.cancel(currentRequest.data.id);
-          setCurrentRequest(undefined);
         }
 
         const request = await publish({
@@ -76,36 +74,56 @@ export const SearchPage = () => {
         });
 
         if (request) {
-          setCurrentRequest(() => request);
+          navigate(`?tour=${tourId}&requestId=${request.data.id}`);
         }
       }
     } catch (err) {
       logger.error(err);
     }
-  }, [currentRequest, publish, requestsManager, selectedDate]);
+  }, [
+    currentRequest,
+    navigate,
+    publish,
+    requestsManager,
+    selectedDate,
+    tourId,
+  ]);
+
+  const handleRequestStop = useCallback(() => {
+    if (requestsManager && currentRequest) {
+      requestsManager.cancel(currentRequest.data.id);
+    }
+  }, [currentRequest, requestsManager]);
 
   const handleRequestCancel = useCallback(() => {
     if (requestsManager && currentRequest) {
-      requestsManager.cancel(currentRequest.data.id);
-      setCurrentRequest(undefined);
-      navigate('/');
+      handleRequestStop();
+      navigate(`?tour=${tourId}`);
     } else {
       navigate('/');
     }
-  }, [currentRequest, navigate, requestsManager]);
+  }, [currentRequest, handleRequestStop, navigate, requestsManager, tourId]);
 
   return (
     <LocalizationProvider dateAdapter={AdapterDayjs}>
       <Container maxWidth="lg" sx={{ paddingTop: 4, paddingBottom: 4 }}>
         {tour && (
-          <Grid container spacing={2} alignItems="flex-start">
+          <Grid
+            container
+            spacing={2}
+            alignItems="flex-start"
+            sx={{ marginBottom: 4 }}
+          >
             <Grid
               item
               xs={12}
               sm={6}
-              sx={{ display: 'flex', justifyContent: 'flex-end' }}
+              sx={{
+                display: 'flex',
+                justifyContent: isMobile ? 'center' : 'flex-end',
+              }}
             >
-              <Card sx={{ maxWidth: 300 }}>
+              <Card sx={{ maxWidth: 300, marginBottom: 2 }}>
                 <CardMedia
                   component="img"
                   height="140"
@@ -122,21 +140,40 @@ export const SearchPage = () => {
                 </CardContent>
               </Card>
             </Grid>
-            <Grid item xs={12} sm={6} container spacing={2}>
+            <Grid
+              item
+              xs={12}
+              sm={6}
+              container
+              spacing={2}
+              sx={{
+                display: 'flex',
+                justifyContent: isMobile ? 'center' : 'flex-start',
+              }}
+            >
               <Grid item>
                 <DatePicker
                   label="Choose a Date"
+                  disabled={Boolean(requestId)}
                   value={dayjs(selectedDate)}
                   onChange={setSelectedDate}
                 />
               </Grid>
-              <Grid item container spacing={1}>
+              <Grid
+                item
+                container
+                spacing={1}
+                sx={{
+                  display: 'flex',
+                  justifyContent: isMobile ? 'center' : 'flex-start',
+                }}
+              >
                 <Grid item>
                   <Button
                     size="large"
                     variant="contained"
                     color="primary"
-                    disabled={!clientConnected}
+                    disabled={!clientConnected || Boolean(requestId)}
                     onClick={handlePublish}
                   >
                     Search
@@ -148,15 +185,26 @@ export const SearchPage = () => {
                     variant="outlined"
                     onClick={handleRequestCancel}
                   >
-                    Cancel
+                    {currentRequest?.subscribed ? 'Stop' : 'Cancel'}
                   </Button>
+                </Grid>
+                <Grid item>
+                  {!clientConnected && (
+                    <Alert severity="warning">
+                      Connecting to the server...
+                    </Alert>
+                  )}
                 </Grid>
               </Grid>
             </Grid>
           </Grid>
         )}
 
-        <Offers offers={currentOffers} />
+        <Offers
+          offers={currentRequest?.offers}
+          subscribed={Boolean(currentRequest?.subscribed)}
+          onStop={handleRequestStop}
+        />
 
         {tourError && <Alert severity="error">{tourError}</Alert>}
         {requestsError && <Alert severity="error">{requestsError}</Alert>}
